db320422a4b4587f1df9cbe0a43c5396d4220211
Timothy J O'Connell <toconnell@tyrannybelle.com>
2015-11-30 10:10:16 -0500

    tuned up admin to be safer and more user friendly. added a remove function for specific documents. prettied up the ls func.

--- v1/admin.py
+++ v1/admin.py
@@ -12,6 +12,9 @@ from utils import mdb, get_logger, get_user_agent
 
 logger = get_logger()
 
+#
+#   administrative helper functions for user-land
+#
 
 def create_new_user(login, password, password_again):
     """ Creates a new user. Returns False if the passwords don't match;
@@ -31,6 +34,7 @@ def create_new_user(login, password, password_again):
             "password": md5(password).hexdigest(),
         }
         mdb.users.insert(user_dict)
+        mdb.users.create_index("login", unique=True)
         return "user created"
 
 
@@ -54,17 +58,34 @@ def authenticate(login, password):
 
 
 #
-#   Interactive CLI admin stuff
+#   Interactive CLI admin stuff; not to be used in user-land
 #
 
 def ls_documents(collection):
-    for d in mdb[collection].find():
-        print d
+    """ Dumps a list- or ls-style rendering of a collection's contents. """
+
+    for d in mdb[collection].find().sort("created_on"):
+        output = " "
+        output += str(d["_id"])
+        output += " - "
+        if collection == "users":
+            output += d["login"]
+        else:
+            output += d["name"]
+        output += " (%s)" % d["created_on"]
+        print output
+
 
 def rm_collection(collection):
+    """ Drops an entire collection from mdb. Serious business. """
+
     mdb[collection].remove()
 
+
 def dump_document(collection, doc_id):
+    """ Prints an object to stdout in a semi-pretty way, e.g. for review or
+    analysis, etc. """
+
     key_length = 31
     document = mdb[collection].find_one({"_id": ObjectId(doc_id)})
     print("")
@@ -72,18 +93,42 @@ def dump_document(collection, doc_id):
         print(" %s%s%s" % (a, " " * (key_length - len(a)), document[a]))
     print("")
 
+
+def remove_document(collection, doc_id):
+    """ Removes a single document (by _id) from the specified collection."""
+
+    doc_id = ObjectId(doc_id)
+    if collection == "settlements":
+        settlement = mdb.settlements.find_one({"_id": doc_id})
+        survivors = mdb.survivors.find({"settlement": doc_id})
+        death_count = 0
+        for s in survivors:
+            mdb.survivors.remove({"_id": s["_id"]})
+            death_count += 1
+        logger.info("[ADMIN] Removed %s survivors." % death_count)
+    mdb[collection].remove({"_id": doc_id})
+    logger.info("[ADMIN] Removed '%s' from mdb.%s" % (doc_id, collection))
+
+
+
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.add_option("-c", "--collection", dest="collection", help="Specify a collection to work with", metavar="settlements", default=False)
+    parser.add_option("-c", dest="collection", help="Specify a collection to work with", metavar="settlements", default=False)
 
-    parser.add_option("-l", "--list", dest="list_documents", help="List documents in a collection", metavar="users", default=False)
-    parser.add_option("-v", "--view", dest="view_document", help="View/dump a document to stdout (requires -c)", metavar="565a1829421aa96b33d1c8bb", default=False)
-    parser.add_option("-R", "--remove_collection", dest="drop_collection", help="Drop all docs in a collection.", metavar="users", default=False)
+    parser.add_option("-l", dest="list_documents", help="List documents in a collection", metavar="survivors", default=False)
+    parser.add_option("-v", dest="view_document", help="View/dump a document to stdout (requires -c)", metavar="565a1829421aa96b33d1c8bb", default=False)
+    parser.add_option("-r", dest="remove_document", help="Remove a single document (requires -c)", metavar="29433d1c8b56581ab21aa96b", default=False)
+    parser.add_option("-R", dest="drop_collection", help="Drop all docs in a collection.", metavar="users", default=False)
     (options, args) = parser.parse_args()
+
 
     if options.list_documents:
         ls_documents(options.list_documents)
     if options.drop_collection:
         rm_collection(options.drop_collection)
-    if options.collection and options.view_document:
-        dump_document(options.collection, options.view_document)
+
+    if options.collection:
+        if options.view_document:
+            dump_document(options.collection, options.view_document)
+        if options.remove_document:
+            remove_document(options.collection, options.remove_document)
