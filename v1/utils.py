f3b2679fc9aacb87264b4386ccf21ae1c8edad30
Timothy J O'Connell <toconnell@tyrannybelle.com>
2015-11-30 09:24:36 -0500

    fixed triplicate logging issue

--- v1/utils.py
+++ v1/utils.py
@@ -55,26 +55,28 @@ def get_logger(log_level="INFO", log_name=False):
 
     #   initialize a generic logger
     logger = logging.getLogger(__name__)
-    exec 'log_level_obj = logging.%s' % log_level
-    logger.setLevel(log_level_obj)
 
-    #   set the logger's name and path
-    log_file_name = log_name
-    if not log_file_name:
-        log_file_name = os.path.splitext(os.path.basename(sys.argv[0]))[0]
+    if not len(logger.handlers):    # if it's not already open, open it
+        exec 'log_level_obj = logging.%s' % log_level
+        logger.setLevel(log_level_obj)
 
-    log_root_dir = settings.get("application","log_dir")
-    if not os.path.isdir(log_root_dir):
-        e = Exception("Logging root dir '%s' does not exist!" % log_root_dir)
-        raise e
+        #   set the logger's name and path
+        log_file_name = log_name
+        if not log_file_name:
+            log_file_name = os.path.splitext(os.path.basename(sys.argv[0]))[0]
 
-    log_path = os.path.join(log_root_dir, log_file_name + ".log")
-    logger_fh = logging.FileHandler(log_path)
+        log_root_dir = settings.get("application","log_dir")
+        if not os.path.isdir(log_root_dir):
+            e = Exception("Logging root dir '%s' does not exist!" % log_root_dir)
+            raise e
 
-    #   set the formatter and add it via addHandler()
-    formatter =  logging.Formatter('[%(asctime)s] %(levelname)s:\t%(message)s',"%Y-%m-%d %H:%M:%S")
-    logger_fh.setFormatter(formatter)
-    logger.addHandler(logger_fh)
+        log_path = os.path.join(log_root_dir, log_file_name + ".log")
+        logger_fh = logging.FileHandler(log_path)
+
+        #   set the formatter and add it via addHandler()
+        formatter =  logging.Formatter('[%(asctime)s] %(levelname)s:\t%(message)s',"%Y-%m-%d %H:%M:%S")
+        logger_fh.setFormatter(formatter)
+        logger.addHandler(logger_fh)
 
     return logger
 
