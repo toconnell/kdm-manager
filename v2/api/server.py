a9eec67e7af39bc7001effdefa530701e2bd3e34
The Watcher <toconnell@tyrannybelle.com>
2016-01-21 21:14:48 -0500

    controllers for fastcgi and ember are now both functional/in place

---
+++ v2/api/server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import cgi
+from flup.server.fcgi import WSGIServer
+import os
+import sys
+import time
+
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+
+    output = '<h1>FastCGI Environment</h1>\n<table>'
+    for k, v in sorted(environ.items()):
+         output += '<tr><th>%s</th><td>%s</td></tr>' % (k,v)
+    output += '</table>'
+    yield output
+
+def start(socket_path="/tmp/server.sock"):
+    WSGIServer(app, bindAddress=socket_path).run()
+
+if __name__ == '__main__':
+    start()
+
---
+++ v2/sysadmin/api_server.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+#   standard lib imports
+import CGIHTTPServer
+import BaseHTTPServer
+import daemon
+import logging
+from lockfile.pidlockfile import PIDLockFile
+import os
+from pwd import getpwuid
+import shutil
+import psutil
+import SimpleHTTPServer
+import SocketServer
+import subprocess
+import sys
+import time
+
+# custom
+import logger
+import settings; settings = settings.load()
+sys.path.append(settings.get("application","project_root"))
+from api import server
+
+
+def start_server():
+    """ Starts a server. If you do this outside of a forked/daemonized process,
+    you can watch it and manually Ctrl-C it. """
+
+    check_pid_dir()
+
+    fcgi_socket = settings.get("api","fcgi_socket")
+    logger.info("Starting API Server on %s..." % fcgi_socket)
+
+    new_pid = os.fork()
+    if new_pid == 0:
+        context = daemon.DaemonContext(
+            detach_process = True,
+            umask=0o002, pidfile=PIDLockFile(settings.get("api", "pid_file")),
+            files_preserve = [logger.handlers[0].stream],
+            )
+        with context:
+            logger.info("PID file location is '%s'" % settings.get("api", "pid_file"))
+            server.start(fcgi_socket)
+
+    time.sleep(3)
+    logger.debug("Server process forked. Setting socket permissions...")
+    os.chmod(fcgi_socket,0777)
+
+
+
+def stop_server():
+    """ Kills a pid. """
+    pid = get_pid()
+    logger.warn("Preparing to kill PID %s" % pid)
+    which_kill = "/bin/kill"
+    p = subprocess.Popen([which_kill, str(pid)], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    logger.critical("Process killed.")
+
+
+def get_pid():
+    """ Gets the current PID associated with the daemon. """
+    try:
+        return int(file(settings.get("api","pid_file"), "rb").read().strip())
+    except:
+        return False
+
+
+def check_pid_dir():
+    """ Checks to see if the PID dir in the settings file exists and
+    raises a generic Exception if it doesn't. """
+
+    pid_dir = os.path.dirname(settings.get("api","pid_file"))
+    if not os.path.isdir(pid_dir):
+        err = "PID dir '%s' does not exist!" % pid_dir
+        logger.critical(err)
+        raise Exception(err)
+    else:
+        logger.info("PID dir '%s' exists." % pid_dir)
+
+    pid_dir_owner = getpwuid(os.stat(pid_dir).st_uid).pw_name
+    logger.info("PID dir '%s' is owned by '%s'." % (pid_dir, pid_dir_owner))
+    if pid_dir_owner != os.environ["USER"]:
+        logger.warn("PID dir owner is not the current user!")
+
+
+if __name__ == "__main__":
+
+    logger = logger.load()
+
+    if not os.path.isfile(settings.get("api","pid_file")):
+        start_server()
+    else:
+        logger.info("PID found. Attempting to stop server...")
+        pid = get_pid()
+        if psutil.pid_exists(pid):
+            stop_server()
+        else:
+            logger.warn("pid file '%s' exists, but PID '%s' does not!" % (settings.get("api","pid_file"), pid))
+            shutil.os.remove(settings.get("api","pid_file"))
+            start_server()
