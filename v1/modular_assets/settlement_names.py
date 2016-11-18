724f103f046ac8a97cd9ec7c67ba3ed3ec2282ec
Timothy J O'Connell <toconnell@tyrannybelle.com>
2016-11-17 19:40:37 -0600

    missed one in 1.49.1029

---
+++ v1/modular_assets/settlement_names.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2.7
+
+core = [
+    "Black Rock",
+    "Beacon",
+    "Ember",
+    "Haven",
+    "Home",
+    "Hope",
+    "Lantern's Reach",
+    "Lux",
+    "Sanctuary",
+    "The Black Lantern",
+    "White Rock",
+]
