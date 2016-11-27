33a77a85a8456d6a9889a921eee6113c743d46c1
Timothy J O'Connell <toconnell@tyrannybelle.com>
2016-11-26 18:54:04 -0600

    bug fix to suppor JSON params to monster route

--- v2/api/routes/monster.py
+++ v2/api/routes/monster.py
@@ -5,14 +5,18 @@ from flask import request, Response
 from models import monster
 import utils
 
+logger = utils.get_logger(log_name="server")
+
 def GET_json():
     """ Simple GET handling for monster assets. Supports lookup by handle or by
     name and returns a 200 if it can find one. Othewise, you get a 404. """
 
-    m_handle = request.args.get("handle", None)
-    m_name = request.args.get("name", None)
-
-    four_oh_four = Response(response=None, status=404, mimetype="application/json")
+    m_handle = None
+    m_name = None
+    if "name" in request.json:
+        m_name = request.json["name"]
+    if "handle" in request.json:
+        m_name = request.json["handle"]
 
     try:
         if m_handle is not None:
@@ -20,9 +24,9 @@ def GET_json():
         elif m_name is not None:
             M = monster.Monster(name=m_name)
         else:
-            return four_oh_four
+            return utils.http_404
     except:
-        return four_oh_four
+        return utils.http_404
 
     r = utils.asset_object_to_json(M)
     return Response(response=r, status=200, mimetype="application/json")
