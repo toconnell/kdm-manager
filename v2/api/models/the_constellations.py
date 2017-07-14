f0683d7d82b2dded4f05242a138297b31d5b1b2e
Timothy O'Connell <toconnell@tyrannybelle.com>
2017-07-14 17:17:11 -0500

    2.22.505

---
+++ v2/api/models/the_constellations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2.7
+
+
+from assets import survivor_sheet_options
+import Models
+
+
+class Assets(Models.AssetCollection):
+
+    def __init__(self, *args, **kwargs):
+        self.assets = survivor_sheet_options.the_constellations
+        self.type = "the_constellations"
+        Models.AssetCollection.__init__(self,  *args, **kwargs)
