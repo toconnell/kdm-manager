759e773c66be427f0070f3918809b5cbe7bdc605
Timothy O'Connell <toconnell@tyrannybelle.com>
2018-05-18 10:49:23 -0500

    2.78.1502

---
+++ v1/js/huntPhase.js
@@ -0,0 +1,85 @@
+app.controller ("huntPhaseRootController", function($scope) {
+    $scope.rollD100 = function(){
+        return Math.floor(Math.random() * 100) + 1;
+    };
+
+    $scope.deck = [];
+    $scope.drawn = []
+
+    $scope.draw = function(){
+        var rand = $scope.deck[Math.floor(Math.random() * $scope.deck.length)];
+        $scope.deck.splice($scope.deck.indexOf(rand), 1);
+        $scope.drawn.push(rand);
+    };
+
+
+    $scope.cards = {
+        random: {
+            handle: 'random_event',
+            name: 'Random Event',
+            subtitle: 'basic hunt event',
+            desc: "Roll d100 on the <b>Hunt Event Table</b>",
+            optional: false,
+            roll_die: true,
+        },
+        baby_and_the_sword: {
+            handle: 'baby_and_the_sword',
+            name: 'Baby and the Sword',
+            subtitle: 'basic hunt event',
+            desc: "The survivors find a woman's corpse riddled with arrows. It rests in the center of a pattern drawn in blood, a screaming infant in one hand and a sword in the other. As the survivors approach, a massive worm bursts from the ground blocking their way! The survivors may Grab & Dash.",
+            optional: true,
+        },
+        object_of_desire: {
+            handle: 'object_of_desire',
+            name: 'Object of Desire',
+            subtitle: 'lonely tree hunt Event',
+            desc: "The survivors see a tree in the distance, reaching up from the horizon like a desparate, gnarled hand. Players may nominate a survivor with 3+ courage to <b>Investigate</b>. If they do, add the <b>Lonley Tree</b> terrain card to the showdown setup and roll on the table.",
+            optional: true,
+        },
+        dead_warrior: {
+            handle: 'dead_warrior',
+            name: 'Dead Warrior',
+            subtitle: 'basic hunt event',
+            desc: "The survivors stop a man's length away from a one-handed skeleton clad in ancient, rusted armor. A strange tablet covered in inscriptions lies next to it. If the settlement has <b>Pictographs</b>, a survivor with 3+ understanding may investigate.",
+            optional: true,
+        },
+    }
+
+    $scope.include = []
+    $scope.includeCard = function(card) {
+
+        if ($scope.include.indexOf(card.handle) == -1) {
+            $scope.include.push(card.handle);
+        } else {
+            $scope.include.splice($scope.include.indexOf(card.handle), 1);
+        };
+
+        if (card.included == true) {
+            card.included = false
+        } else {
+            card.included = true
+        };
+        console.warn(card.included);
+    };
+
+    $scope.shuffleDeck = function() {
+        $scope.deck = [];
+        $scope.drawn = [];
+
+        var random = 12;
+        for(var i=0; i < random; i++){
+            var randomCard = jQuery.extend({}, $scope.cards.random);
+            randomCard.die_roll = $scope.rollD100();
+            $scope.deck.push(randomCard);
+        };
+        for(var i=0; i < $scope.include.length; i++){
+            var handle = $scope.include[i];
+            $scope.deck.push($scope.cards[handle]);
+        };
+
+
+        console.warn($scope.include);
+    };
+
+
+});
---
+++ v1/js/login.js
@@ -0,0 +1,224 @@
+var app = angular.module('login', []);
+
+app.controller("globalController", function($scope) {
+
+    // pressing "enter" while focusing on the "password" input submits
+    // the sign in "form" (which isn't a form).
+    var pw_input = document.getElementById("signInPassword");
+    pw_input.addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("signInButton").click();
+        }
+    });
+
+    $scope.hideControls = function(element_id) {
+        var controls = document.getElementById(element_id);
+        controls.classList.add('hidden');
+    };
+
+    $scope.showControls = function(element_id) {
+        var controls = document.getElementById(element_id);
+        controls.classList.remove('hidden');
+    };
+
+    $scope.showSignInControls = function() {
+        $scope.hideControls('help_controls');
+        $scope.hideControls('new_user_controls');
+        $scope.showControls('sign_in_controls');
+        document.getElementById("signInEmail").focus()
+    };    
+
+    $scope.showNewUserControls = function() {
+        $scope.hideControls('sign_in_controls');
+        $scope.hideControls('help_controls');
+        $scope.showControls('new_user_controls');
+        document.getElementById("newUserEmail").focus()
+    };    
+
+    $scope.showHelpControls = function() {
+        $scope.showControls('help_controls');
+        document.getElementById("resetPasswordEmail").focus()
+    };    
+
+    $scope.loading = function(action) {
+        if (action === undefined) {
+            $scope.hideControls('sign_in_controls');
+            $scope.hideControls('new_user_controls');
+            $scope.hideControls('help_controls');
+            $scope.showControls('loading_spinner');
+        } else {
+            $scope.hideControls('loading_spinner');
+            $scope.showControls('sign_in_controls');
+        };
+    };
+
+
+    $scope.legacySignIn = function(un, pw) {
+        // signs into the legacy webapp by emulating a form POST
+        var form = document.createElement("form");
+        form.method = "POST";
+        form.action = "/";   
+
+        var username = document.createElement("input"); 
+        var password = document.createElement("input"); 
+
+        username.name = 'login';
+        username.value = un;
+        username.classList.add('hidden');
+        form.appendChild(username);
+
+        password.name = 'password';
+        password.value = pw;
+        password.classList.add('hidden');
+        form.appendChild(password);  
+
+        document.body.appendChild(form);
+        form.submit();
+
+    };
+});
+
+app.controller("signInController", function($scope, $http) {
+    $scope.signIn = function(api_url) {
+        $scope.loading();
+        var data = {username: $scope.signInEmail, password: $scope.signInPassword};
+        $http({
+            method: 'POST',
+            url: api_url + "login",
+            data: data
+        }).then(function successCallback(response) {
+                var r = response.data;
+                console.log("Authentication successful! Initiating legacy webapp sign-in...");
+                $scope.legacySignIn($scope.signInEmail, $scope.signInPassword);
+                return
+            }, function errorCallback(response) {
+                console.error("signIn('" + api_url + "') method failed!");
+                console.error("Response was: " + response.data + "; status was: " + response.status);
+                $scope.loading('off');
+                if (response.status === -1) {
+                    console.error("API is unavailable or unreachable!");
+                    $scope.showControls('api_unavailable');
+                } else {
+                    $scope.showControls('sign_in_error');
+                };
+            });
+        };
+
+});
+
+app.controller("newUserController", function($scope, $http) {
+    $scope.register = function(api_url) {
+        $scope.loading();
+        if ($scope.newUserPassword !== $scope.newUserPasswordAgain) {
+            $scope.showControls('pw_match_error');
+            $scope.loading('off');
+            $scope.showNewUserControls();
+            return
+        } else if ($scope.newUserEmail === undefined) {
+            $scope.showControls('new_user_error');
+            $scope.loading('off');
+            var error_div = document.getElementById('new_user_error_alert');
+            error_div.innerHTML = 'Please enter a valid email address!';
+            $scope.showNewUserControls();
+            return
+        };
+        // if we're still here after validation, attempt to register:
+        var data = {username: $scope.newUserEmail, password: $scope.newUserPassword};
+//        console.log(data);
+        $http({
+            method: 'POST',
+            url: api_url + "new/user",
+            data: data,
+        }).then(function successCallback(response) {
+                var r = response.data;
+//                console.log(r);
+                $scope.legacySignIn($scope.newUserEmail, $scope.newUserPassword);
+                return
+            }, function errorCallback(response) {
+                var r = response.data;
+                console.error(r);
+                $scope.loading('off');
+                var error_div = document.getElementById('new_user_error_alert');
+                error_div.innerHTML = r;
+                $scope.showControls('new_user_error');
+            }
+        );
+    };
+});
+
+app.controller("resetPasswordController", function($scope, $http) {
+    $scope.loading = function(action) {
+        if (action === undefined) {
+            $scope.hideControls('reset_password_controls');
+            $scope.showControls('loading_spinner');
+        } else {
+            $scope.hideControls('loading_spinner');
+            $scope.showControls('reset_password_controls');
+        };
+    };
+    $scope.reset = function(api_url) {
+        $scope.loading();
+        if ($scope.newPassword !== $scope.newPasswordAgain) {
+            $scope.showControls('pw_match_error');
+            $scope.loading('off');
+            $scope.showNewUserControls();
+            return
+        };
+        var un = document.getElementById('resetPasswordEmail').value;
+        var r_code = document.getElementById('recoveryCode').value;
+        data = {
+            'username': un,
+            'password': $scope.newPassword,
+            'recovery_code': r_code,
+        }
+        $http({
+            method: 'POST',
+            url: api_url + "reset_password/reset",
+            data: data,
+        }).then(function successCallback(response) {
+                $scope.legacySignIn(un, $scope.newPassword);
+            }, function errorCallback(response) {
+                var r = response;
+                console.error(r);
+                var error_div = document.getElementById('help_error_alert');
+                error_div.innerHTML = r.data;
+                $scope.showControls('help_error');
+                $scope.loading('off');
+                return
+            }
+        );
+    };
+});
+
+app.controller("helpController", function($scope, $http) {
+    $scope.resetPassword = function(api_url) {
+        $scope.loading();
+        data = {'username': $scope.resetPasswordEmail};
+        $http({
+            method: 'POST',
+            url: api_url + "reset_password/request_code",
+            data: data,
+        }).then(function successCallback(response) {
+                $scope.loading('off');
+                var r = response.data;
+                var success_p = document.getElementById('successMessage');
+                success_p.innerHTML = 'Am email containing further instructions has been sent to <b>' + $scope.resetPasswordEmail + '</b>.';
+                $scope.showControls('help_success');
+                return
+            }, function errorCallback(response) {
+                $scope.loading('off');
+                var r = response;
+                console.error(r);
+                var error_div = document.getElementById('help_error_alert');
+                error_div.innerHTML = r.data;
+                $scope.showControls('help_error');
+                return
+            }
+        );
+    };
+});
+
+
+
+
